6cceaa834175b95ad12fa2eee3834a2d06dbf019
TecnologySoft <98361017+AdrianCadillo@users.noreply.github.com>
2023-10-12 18:06:43 -0500

    Add files via upload

---
+++ port.php
@@ -0,0 +1,17 @@
+<?php
+ /** Cuando hablamos de estract 
+  * Covertir a partir de un array asociativo, las key o clves se conviertena a variables
+ */
+
+ //$courses = ["name"=>"JAVA DEVELOPER","price"=>"220","categoria"=>"DESARROLLO WEB"];
+
+ /// aplicamos el stract
+
+ //extract($courses);
+
+ /**
+  * TEMA: compact() -> convierte variables a array asociativo
+  */
+
+   
+?>
